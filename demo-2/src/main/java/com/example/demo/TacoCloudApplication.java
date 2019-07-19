9b4c493ded964160db1d010d01602c992752e3a7
LENOVO <LENOVO@DESKTOP-MFOVQ9U>
2019-07-19 00:30:40 -0400

    all

---
+++ demo-2/src/main/java/com/example/demo/TacoCloudApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+    public static void main(String[] args) {
+	SpringApplication.run(TacoCloudApplication.class,args);
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ demo-2/src/test/java/com/example/demo/TacoCloudApplicationTest1.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Test;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class TacoCloudApplicationTest1 {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("hi there");
+	}
+
+}
